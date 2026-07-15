9970dbd66c9ff902d7a6f6ab580aa8c25e610c20
jklorenz <jakecartilla@gmail.com>
2026-07-15 19:18:04 +0800

    feat: add AdminSeeder and wire DatabaseSeeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::firstOrCreate(
+            ['email' => env('ADMIN_EMAIL', 'admin@townlink.local')],
+            [
+                'name' => 'Administrator',
+                'password' => Hash::make(env('ADMIN_PASSWORD', 'password123')),
+            ]
+        );
+    }
+}
